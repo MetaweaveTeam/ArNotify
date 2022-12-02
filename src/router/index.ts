ba8c836f354bc6d6498ebe90221b5795ee8ccbca
André Nunes <hello@anuness.dev>
2022-12-02 18:16:02 +0000

    fix routes for arweave

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,20 +2,38 @@ import { createRouter, createWebHistory } from "vue-router";
 import IndexView from "@/views/IndexView.vue";
 import ErrorView from "@/views/ErrorView.vue";
 
+const routes = () => {
+  if (process.env.NODE_ENV === "development") {
+    return [
+      {
+        path: "/",
+        name: "home",
+        component: IndexView,
+      },
+      {
+        path: "/error",
+        name: "error",
+        component: ErrorView,
+      },
+    ];
+  } else {
+    return [
+      {
+        path: "/:txid?",
+        name: "home",
+        component: IndexView,
+      },
+      {
+        path: "/:txid/error",
+        name: "error",
+        component: ErrorView,
+      },
+    ];
+  }
+};
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: IndexView,
-    },
-    {
-      path: "/error",
-      name: "error",
-      component: ErrorView,
-    },
-  ],
+  routes: routes(),
 });
 
 export default router;
